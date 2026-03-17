f02a82652bd539bf059dabea91141ec56c34583e
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-03-18 04:00:26 +0900

    feat: buid update

---
+++ public/build/assets/app-layout-XeBGU_VV.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-BfnD9TCM.js";import{A as s}from"./app-sidebar-layout-xPAQlwcg.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};

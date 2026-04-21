1a8b24769e72e6f174cfde05e7eeb2984b893628
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-04-21 07:24:43 +0700

    feat: update build

--- public/build/assets/app-layout-CNz572X8.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DUOSQ1qb.js";import{A as s}from"./app-sidebar-layout-BDujefpt.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/app-layout-DRzwrink.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CZf4Mp98.js";import{A as s}from"./app-sidebar-layout-Bh7fHOl3.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};

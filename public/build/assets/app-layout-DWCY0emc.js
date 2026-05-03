a467e0a2a75640aad788312b3f3b889e59a3a07a
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-05-03 10:38:20 +0700

    feat: build frontend assets and add database and access control documentation

---
+++ public/build/assets/app-layout-DWCY0emc.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-TOacNO71.js";import{A as s}from"./app-sidebar-layout-DScXCQYa.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};

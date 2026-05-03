2c8e84ed4c5fce6a682908625de003ab3d103083
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-05-03 15:13:55 +0700

    update build

--- public/build/assets/app-layout-COm9-OPz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-B6HSjz06.js";import{A as s}from"./app-sidebar-layout-TUIm28cr.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};

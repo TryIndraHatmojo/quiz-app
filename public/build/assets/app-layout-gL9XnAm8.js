2ea66b9123938d4e34f490c92890a6ea2564f616
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-05-19 05:33:34 +0700

    update build

---
+++ public/build/assets/app-layout-gL9XnAm8.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CfPFqg_w.js";import{A as s}from"./app-sidebar-layout-Dfo9dCk5.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};

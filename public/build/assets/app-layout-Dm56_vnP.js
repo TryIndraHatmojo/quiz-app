965fc4e1411e021631cc0ba9cf04a66a1c250c93
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-05-31 19:30:52 +0700

    update build

---
+++ public/build/assets/app-layout-Dm56_vnP.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-D8V0kSBu.js";import{A as s}from"./app-sidebar-layout-Cu4kSoq1.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};

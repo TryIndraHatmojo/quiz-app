2f889d4685a185c6dd8a9dae8fcc849ac8c3394f
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-05-31 08:36:01 +0700

    update build

--- public/build/assets/app-layout-BZnxkabf.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-fsr64NiK.js";import{A as s}from"./app-sidebar-layout-glKhUCCF.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};

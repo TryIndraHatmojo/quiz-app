6d097d3d2a3fe615b69b39e5cbd174b92a1d77e5
Try Indra Hatmojo <tryindra3375@gmail.com>
2026-06-14 08:50:34 +0700

    update build

---
+++ public/build/assets/app-layout-BPsPzsnL.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-tiuWtTkD.js";import{A as s}from"./app-sidebar-layout-BNJ8ZnTa.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
